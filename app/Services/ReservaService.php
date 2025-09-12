ea0d2ead1bae3368ef369a8b601a0bbad0502d0c
Brayan Vargas <brayanvargasbarajas@gmail.com>
2025-09-12 16:20:23 -0500

    Ajustar el formato de la fecha de creación en getMiReserva para utilizar el formato de 12 horas.

--- app/Services/ReservaService.php
+++ app/Services/ReservaService.php
@@ -1982,7 +1982,7 @@ class ReservaService
                 'mensualidad' => $coberturaMensualidad['mensualidad'],
                 // Información de pago con detalles (polimórfica por tipo_concepto)
                 'pago' => $pagoResumen,
-                'creado_en' => Carbon::parse($reserva->creado_en)->format('d/m/Y H:i A')
+                'creado_en' => Carbon::parse($reserva->creado_en)->format('d/m/Y h:i A'),
             ];
 
             return $resumenReserva;
