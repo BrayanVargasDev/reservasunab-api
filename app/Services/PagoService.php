b52337c3dc32e3ee790dcb137e587806eb60e973
Brayan Vargas <brayanvargasbarajas@gmail.com>
2025-10-10 17:00:36 -0500

    Mejora de url desde ios

--- app/Services/PagoService.php
+++ app/Services/PagoService.php
@@ -250,7 +250,7 @@ class PagoService
 
             $pago = $this->crearPago($id_reserva);
             $url_base = $this->url_redirect_base;
-            $url_redirect = $url_base . '?codigo=' . $pago->codigo . 'desde_ios=' . $desde_ios;
+            $url_redirect = $url_base . '?codigo=' . $pago->codigo . '&desde_ios=' . $desde_ios;
             Log::info("Redirigiendo a: $url_redirect");
             $this->getSessionToken();
             $data['SessionToken'] = $this->session_token;
